bde5e092447e9af3a862719c7a89962067b04574
Sudarshan Gadekar <sudarshangadekar222@gmail.com>
2025-03-19 21:16:03 +0530

    remove unnessasory files

--- src/App.js
+++ src/App.js
@@ -11,8 +11,6 @@ function App() {
     <>
     <Routes>
       <Route path="/" element = {<Home/>}/>
-      <Route path="/about" element = {<About/>}/>
-      <Route path="/contact" element = {<Contact/>}/>
     </Routes>
     </>
   );
